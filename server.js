b0d987a6d6405648948760a8564b63bcf7f7c05e
Artemka0-7 <pavlovskiy.07@inbox.ru>
2022-05-09 16:35:31 +0300

    addings

--- server.js
+++ server.js
@@ -6,23 +6,24 @@ const mongoose = require('mongoose');
 const nameRoutes = require('./routes/name-routes')
 const {createPath, handleError} = require('./helpers/helper');
 const chalk = require('chalk');
-const successMsg = chalk.bgKeyword('green').white; 
+const successMsg = chalk.bgKeyword('green').white.bold; 
 const errorMsg = chalk.bgKeyword('white').red; 
+const reqMsg = chalk.bgKeyword('purple').cyan.bold.underline; 
 
 const app = express();
 app.set('view engine', 'ejs');  
 
-mongoose  
+/*mongoose  
   .connect(process.env.MONGO_URL)  
   .then((res) => console.log(successMsg('Connected to DB'))) 
-  .catch((error) => console.log(errorMsg(error))); 
+  .catch((error) => console.log(errorMsg(error))); */
 
 app.listen(process.env.PORT,  (error) => {
   error ? console.log(errorMsg(error)) : console.log(successMsg(`Listening port ${process.env.PORT}`));
 }); 
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms')) 
-app.use(methodOverride('_method'))  
+app.use(morgan(reqMsg(':method :url :status :res[content-length] - :response-time ms'))); 
+app.use(methodOverride('_method'));  
 app.use(express.urlencoded({extended: false})); 
 app.use(express.static('styles')); 
 
